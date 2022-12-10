a42b7bb050d6732e716b5d7b8da3a16ecb5b1c80
Anthony Hall <anthonyhall@Anthonys-MBP.fios-router.home>
2022-12-09 21:52:00 -0500

    Added the option to select a new Destination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
-Destinations = ["Gettysburg", "Philadelphia", "York", "Ocean City", "Washington DC"]
-print (random.choice(Destinations))
+Destination = ["Gettysburg", "Philadelphia", "York", "Ocean City", "Washington DC"]
+print (random.choice(Destination))
 
 import random
 Restuarants = ["Dough Roller", "Pizza Hut", "Jimmy's", "Chili's"]
@@ -13,3 +13,14 @@ print (random.choice(Mode_of_Transportation))
 import random
 Entertainment = ["Museum", "Movie", "Hiking", "Guided Tour"]
 print (random.choice(Entertainment))
+
+Question_1 = input("Do you like all your choices? ")
+if Question_1 == "Yes":
+    print("Awesome, have a great Trip!")
+elif Question_1 == "No":
+    print (next)
+Question_2 = input("Which option do you not like? ")
+if Question_2 == ("The Destination"):
+    import random
+    Destination = ["Gettysburg", "Philadelphia", "York", "Ocean City", "Washington DC"]
+    print (random.choice(Destination))
