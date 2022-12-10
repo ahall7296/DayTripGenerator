a7cad62f66b1e3a0f2f550d18472ffe3245baba5
Anthony Hall <anthonyhall@Anthonys-MBP.fios-router.home>
2022-12-10 08:11:19 -0500

    Added the option to choose different valkues for the questions

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ Destination = ["Gettysburg", "Philadelphia", "York", "Ocean City", "Washington D
 print (random.choice(Destination))
 
 import random
-Restuarants = ["Dough Roller", "Pizza Hut", "Jimmy's", "Chili's"]
-print (random.choice(Restuarants))
+Restuarant = ["Dough Roller", "Pizza Hut", "Jimmy's", "Chili's"]
+print (random.choice(Restuarant))
 
 import random
 Mode_of_Transportation = ["Car", "Bus", "Plain"]
@@ -18,9 +18,23 @@ Question_1 = input("Do you like all your choices? ")
 if Question_1 == "Yes":
     print("Awesome, have a great Trip!")
 elif Question_1 == "No":
-    print (next)
+    next
 Question_2 = input("Which option do you not like? ")
-if Question_2 == ("The Destination"):
+if Question_2 == ("The destination"):
     import random
     Destination = ["Gettysburg", "Philadelphia", "York", "Ocean City", "Washington DC"]
     print (random.choice(Destination))
+elif Question_2 == ("The restuarant"):
+    import random
+    Restuarant = ["Dough Roller", "Pizza Hut", "Jimmy's", "Chili's"]
+    print (random.choice(Restuarant))
+elif Question_2 == ("The mode of transportation"):
+    import random
+    Mode_of_Transportation = ["Car", "Bus", "Plain"]
+    print (random.choice(Mode_of_Transportation))
+elif Question_2 == ("The entertainment"):
+    import random
+    Entertainment = ["Museum", "Movie", "Hiking", "Guided Tour"]
+    print (random.choice(Entertainment))
+
+
