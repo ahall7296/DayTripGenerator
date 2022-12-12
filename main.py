b4b14335e5f9892fddcd5a1b2a73f2e5649f3e84
Anthony Hall <anthonyhall@Anthonys-MBP.fios-router.home>
2022-12-11 20:47:07 -0500

    Added the option to continually reselect trip options.

--- main.py
+++ main.py
@@ -19,22 +19,28 @@ if Question_1 == "Yes":
     print("Awesome, have a great Trip!")
 elif Question_1 == "No":
     next
-Question_2 = input("Which option do you not like? ")
-if Question_2 == ("The destination"):
-    import random
-    Destination = ["Gettysburg", "Philadelphia", "York", "Ocean City", "Washington DC"]
-    print (random.choice(Destination))
-elif Question_2 == ("The restuarant"):
-    import random
-    Restuarant = ["Dough Roller", "Pizza Hut", "Jimmy's", "Chili's"]
-    print (random.choice(Restuarant))
-elif Question_2 == ("The mode of transportation"):
-    import random
-    Mode_of_Transportation = ["Car", "Bus", "Plain"]
-    print (random.choice(Mode_of_Transportation))
-elif Question_2 == ("The entertainment"):
-    import random
-    Entertainment = ["Museum", "Movie", "Hiking", "Guided Tour"]
-    print (random.choice(Entertainment))
+    def main():
+        Question_2 = input("Which option do you not like? ")
+        if Question_2 == ("The destination"):
+            import random
+            Destination = ["Gettysburg", "Philadelphia", "York", "Ocean City", "Washington DC"]
+            print (random.choice(Destination))
+        elif Question_2 == ("The restuarant"):
+            import random
+            Restuarant = ["Dough Roller", "Pizza Hut", "Jimmy's", "Chili's"]
+            print (random.choice(Restuarant))
+        elif Question_2 == ("The mode of transportation"):
+            import random
+            Mode_of_Transportation = ["Car", "Bus", "Plain"]
+            print (random.choice(Mode_of_Transportation))
+        elif Question_2 == ("The entertainment"):
+            import random
+            Entertainment = ["Museum", "Movie", "Hiking", "Guided Tour"]
+            print (random.choice(Entertainment))
+        Question_3 = input("Do you like all your options now? ")
+        if Question_3 == ("Yes"):
+            print("Awesome, have a great Trip!")
+        elif Question_3 == ("No"):
+            main()
 
-
+main ()
