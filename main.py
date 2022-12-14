2b725a7bff119336bd2359c2d5706cd214054a6c
Anthony Hall <anthonyhall@Anthonys-MBP.fios-router.home>
2022-12-13 19:03:32 -0500

    Added descriptive messages for the user results.

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import random
+
 Destination = ["Gettysburg", "Philadelphia", "York", "Ocean City", "Washington DC"]
-print (random.choice(Destination))
+print ("You are going to", random.choice(Destination))
 
-import random
 Restuarant = ["Dough Roller", "Pizza Hut", "Jimmy's", "Chili's"]
-print (random.choice(Restuarant))
+print ("You are eating at", random.choice(Restuarant))
 
-import random
 Mode_of_Transportation = ["Car", "Bus", "Plain"]
-print (random.choice(Mode_of_Transportation))
+print ("You will be getting there by", random.choice(Mode_of_Transportation))
 
-import random
 Entertainment = ["Museum", "Movie", "Hiking", "Guided Tour"]
-print (random.choice(Entertainment))
+print ("Your entertainment will be", random.choice(Entertainment))
 
 Question_1 = input("Do you like all your choices? ")
 if Question_1 == "Yes":
@@ -22,21 +20,17 @@ elif Question_1 == "No":
     def main():
         Question_2 = input("Which option do you not like? ")
         if Question_2 == ("The destination"):
-            import random
             Destination = ["Gettysburg", "Philadelphia", "York", "Ocean City", "Washington DC"]
-            print (random.choice(Destination))
+            print ("You are going to", random.choice(Destination))
         elif Question_2 == ("The restuarant"):
-            import random
             Restuarant = ["Dough Roller", "Pizza Hut", "Jimmy's", "Chili's"]
-            print (random.choice(Restuarant))
+            print ("You are eating at", random.choice(Restuarant))
         elif Question_2 == ("The mode of transportation"):
-            import random
             Mode_of_Transportation = ["Car", "Bus", "Plain"]
-            print (random.choice(Mode_of_Transportation))
+            print ("You will be getting there by", random.choice(Mode_of_Transportation))
         elif Question_2 == ("The entertainment"):
-            import random
             Entertainment = ["Museum", "Movie", "Hiking", "Guided Tour"]
-            print (random.choice(Entertainment))
+            print ("Your entertainment will be", random.choice(Entertainment))
         Question_3 = input("Do you like all your options now? ")
         if Question_3 == ("Yes"):
             print("Awesome, have a great Trip!")
